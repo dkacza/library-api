2a06404f5c94857c9d29d6d53d4503844b0930a5
Dawid Kacza <dawidkacza02@gmail.com>
2023-04-05 11:35:38 +0200

    Implemented fallback error handling
    Fixed DB connection. Use IPv4 loopback address.

--- server.mjs
+++ server.mjs
@@ -2,21 +2,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import morgan from 'morgan';
 
-// Routers
+// Modules
 import bookRouter from './routes/bookRouter.mjs';
 import userRouter from './routes/userRouter.mjs';
 import authorRouter from './routes/authorRouter.mjs';
 
+process.on('uncaughtException', (err) => {
+    console.log('Unhandled Exception. Exiting...');
+    console.log(err);
+    process.exit(1);
+})
+
 dotenv.config({path: './config.env'});
 
 // Establishing DB connection:
+console.log(process.env.DB);
 mongoose.connect(process.env.DB, {})
-    .then(() => console.log('DB connection established.'))
-    .catch(() => console.log('DB connection error'));
+    .then(() => console.log('DB connection established.'));
 
 
 const app = express();
+
+// Middleware stack
+app.use(morgan('tiny'));
 
 // Monting routers
 app.use('/api/v1/books', bookRouter);
@@ -27,3 +37,9 @@ const port = Number(process.env.PORT);
 app.listen(port, () => {
     console.log(`Server is running at port ${port}.`);
 });
+
+process.on('unhandledRejection', (err) => {
+    console.log('Unhandled Rejection. Exiting...');
+    console.log(err);
+    process.exit(1);
+})
