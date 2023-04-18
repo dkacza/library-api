f9a9b6c5482974e468992f3d6c68fb8c8fe7cf10
Dawid Kacza <dawidkacza02@gmail.com>
2023-04-18 16:40:11 +0200

    Implemented rental model

--- models/rental.mjs
+++ models/rental.mjs
@@ -2,22 +2,30 @@ import mongoose from 'mongoose';
 
 const rentalSchema = new mongoose.Schema({
     book: {
-        // REFERENCE TO THE BOOK
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+        required: [true, 'Book ID is required in rental object'],
     },
     user: {
-        // REFERENCE TO THE USER
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User ID is required in rental object']
     },
     startDate: {
-
+        type: Date,
+        default: Date.now(),
     },
-    endDate: {
-
+    returnDate: {
+        type: Date,
     },
     expirationDate: {
-
+        type: Date,
+        default: this.startDate + 20,
     },
     currentStatus: {
-
+        type: String,
+        enum: ['returned', 'lost', 'borrowed'],
+        default: ['borrowed'],
     },
 });
 
