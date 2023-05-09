7de48aedbadac4542f9c907bc9bdab03f8ef29d2
Dawid Kacza <dawidkacza02@gmail.com>
2023-05-09 11:07:51 +0200

    Added bash script for importing sample data

---
+++ dev-data/importData.bash
@@ -0,0 +1,12 @@
+MONGO_HOST="localhost"
+MONGO_PORT="27017"
+DATABASE_NAME="library"
+COLLECTION_NAME="users"
+JSON_FILE="users.json"
+
+mongoimport --host $MONGO_HOST --port $MONGO_PORT --db $DATABASE_NAME --collection $COLLECTION_NAME --file $JSON_FILE --jsonArray
+
+COLLECTION_NAME="books"
+JSON_FILE="books.json"
+
+mongoimport --host $MONGO_HOST --port $MONGO_PORT --db $DATABASE_NAME --collection $COLLECTION_NAME --file $JSON_FILE --jsonArray
