7a2aebadcb23c76f26bd71073a951766235292ed
Dawid Kacza <dawidkacza02@gmail.com>
2023-05-09 11:02:19 +0200

    Implemented sample data importing

---
+++ dev-data/importData.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set MONGO_HOST=localhost
+set MONGO_PORT=27017
+set DATABASE_NAME=library
+set COLLECTION_NAME=users
+set JSON_FILE=users.json
+
+mongoimport --host %MONGO_HOST% --port %MONGO_PORT% --db %DATABASE_NAME% --collection %COLLECTION_NAME% --file %JSON_FILE% --jsonArray
+
+set COLLECTION_NAME=books
+set JSON_FILE=books.json
+
+mongoimport --host %MONGO_HOST% --port %MONGO_PORT% --db %DATABASE_NAME% --collection %COLLECTION_NAME% --file %JSON_FILE% --jsonArray
