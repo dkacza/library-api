1aea34e54030a76b12fd7cdb19761ec6b4e4d05a
Dawid Kacza <dawidkacza02@gmail.com>
2023-08-07 17:24:51 +0200

    Refactored aggregation module

--- aggregations/rentalAggregation.mjs
+++ aggregations/rentalAggregation.mjs
@@ -1,130 +1,201 @@
 import isEmptyObject from '../utils/isEmptyObject.mjs';
 import Rental from '../models/rental.mjs';
 
-const rentalAggregation = function([searchFields], query, user = undefined ) {
+const rentalAggregation = function(query, user = undefined) {
 
-    let queryString = JSON.stringify(query);
-    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    const parsedQuery = JSON.parse(queryString);
+  let queryString = JSON.stringify(query);
+  queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+  const parsedQuery = JSON.parse(queryString);
 
-    // Joining and projecting
-    let userIdStage = {};
-    if (user) {
-        userIdStage = {$match: {user: user._id}};
+  // Joining and projecting
+  let userIdStage = {};
+  if (user.role === 'user') {
+    userIdStage = { $match: { user: user._id } };
+  }
+  const joinBooksStage = [
+    {
+      $lookup: {
+        from: 'books',
+        localField: 'book',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $project: {
+              title: 1,
+              isbn: 1
+            }
+          }
+        ],
+        as: 'bookData'
+      }
+    },
+    { $unwind: '$bookData' }
+  ];
+
+  const projectBookFieldsStage = [{
+    $set: {
+      'title': '$bookData.title',
+      'isbn': '$bookData.isbn'
     }
-    const joinBooksStage = [
-        {
-            $lookup: {
-                from: 'books',
-                localField: 'book',
-                foreignField: '_id',
-                pipeline: [
-                    {
-                        $project: {
-                            title: 1,
-                            isbn: 1
-                        }
-                    }
-                ],
-                as: 'bookData'
-            }
-        },
-        {$unwind: '$bookData'}
-    ];
-
-    const projectBookFieldsStage = [{
-        $set: {
-            'title': '$bookData.title',
-            'isbn': '$bookData.isbn'
-        }
-    }, {
-        $project: {
-            'bookData': 0
-        }
-    }];
-
-    // Filters
-    let filterByStatusStage = {};
-    if (parsedQuery.currentStatus) {
-        filterByStatusStage = {
-            $match: {
-                $or: []
-            }
-        };
-        const statuses = parsedQuery.currentStatus.split(',');
-        statuses.forEach(status => {
-            filterByStatusStage.$match.$or.push({currentStatus: status});
-        });
+  }, {
+    $project: {
+      'bookData': 0
     }
+  }];
 
-    let filterByStartDateStage = {};
-    if (parsedQuery.startDate ) {
-        filterByStartDateStage = {
-            $match: {
-                startDate: {}
+  const joinUsersStage = [
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'user',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $project: {
+              firstName: 1,
+              lastName: 1,
+              email: 1,
+              phoneNumber: 1
             }
-        };
-        filterByStartDateStage.$match.startDate = (parsedQuery.startDate);
-        for (const [key, val] of Object.entries(filterByStartDateStage.$match.startDate)) {
-            filterByStartDateStage.$match.startDate[key] = new Date(val);
-        }
+          }
+        ],
+        as: 'userData'
+      }
+    },
+    { $unwind: '$userData' }
+  ];
+
+  const projectUsersFieldsStage = [{
+    $set: {
+      'firstName': '$userData.firstName',
+      'lastName': '$userData.lastName',
+      'email': '$userData.email',
+      'phoneNumber': '$userData.phoneNumber'
     }
-
-    let filterByReturnDateStage = {};
-    if (parsedQuery.returnDate) {
-        filterByReturnDateStage = {
-            $match: {
-                returnDate: {}
-            }
-        }
-        filterByReturnDateStage.$match.returnDate = (parsedQuery.returnDate);
-        for (const [key, val] of Object.entries(filterByReturnDateStage.$match.returnDate)) {
-            filterByReturnDateStage.$match.returnDate[key] = new Date(val);
-        }
+  }, {
+    $project: {
+      'userData': 0
     }
+  }];
 
-    let searchByTitleStage = {};
-    if (parsedQuery.search) {
-        searchByTitleStage = {
-            $match: {
-                title: {$regex: new RegExp(parsedQuery.search, 'i')}
-            }
-        }
+  // Filters
+  let filterByStatusStage = {};
+  if (parsedQuery.currentStatus) {
+    filterByStatusStage = {
+      $match: {
+        $or: []
+      }
+    };
+    const statuses = parsedQuery.currentStatus.split(',');
+    statuses.forEach(status => {
+      filterByStatusStage.$match.$or.push({ currentStatus: status });
+    });
+  }
+
+  let filterByStartDateStage = {};
+  if (parsedQuery.startDate) {
+    filterByStartDateStage = {
+      $match: {
+        startDate: {}
+      }
+    };
+    filterByStartDateStage.$match.startDate = (parsedQuery.startDate);
+    for (const [key, val] of Object.entries(filterByStartDateStage.$match.startDate)) {
+      filterByStartDateStage.$match.startDate[key] = new Date(val);
     }
+  }
 
-    let paginationStages = [];
-    if (parsedQuery.page && parsedQuery.limit) {
-        const page = Number(parsedQuery.page) || 1;
-        const limit = Number(parsedQuery.limit) || 100;
-        const toSkip = (page - 1) * limit;
-        paginationStages.push({
-            $facet: {
-                paginatedResults: [
-                    {$skip: toSkip},
-                    {$limit: limit}
-                ],
-                totalCount: [
-                    {$count: 'count'}
-                ]
-            }
-        });
-        paginationStages.push({
-            $unwind: {
-                path: '$totalCount'
-            }
-        });
+  let filterByReturnDateStage = {};
+  if (parsedQuery.returnDate) {
+    filterByReturnDateStage = {
+      $match: {
+        returnDate: {}
+      }
+    };
+    filterByReturnDateStage.$match.returnDate = (parsedQuery.returnDate);
+    for (const [key, val] of Object.entries(filterByReturnDateStage.$match.returnDate)) {
+      filterByReturnDateStage.$match.returnDate[key] = new Date(val);
     }
+  }
 
-    const aggregationStages = [];
-    if (!isEmptyObject(filterByStatusStage)) aggregationStages.push(userIdStage);
-    if (!isEmptyObject(filterByStatusStage)) aggregationStages.push(filterByStatusStage);
-    if (!isEmptyObject(filterByStartDateStage)) aggregationStages.push(filterByStartDateStage);
-    if (!isEmptyObject(filterByReturnDateStage)) aggregationStages.push(filterByReturnDateStage);
-    aggregationStages.push(...joinBooksStage);
-    aggregationStages.push(...projectBookFieldsStage);
-    if (!isEmptyObject(searchByTitleStage)) aggregationStages.push(searchByTitleStage);
-    aggregationStages.push(...paginationStages);
+  let searchByBookStage = {};
+  if (parsedQuery.bookSearch) {
+    searchByBookStage = {
+      $match: {
+        $or: [
+          { title: { $regex: new RegExp(parsedQuery.bookSearch, 'i') } },
+          { isbn: { $regex: new RegExp(parsedQuery.bookSearch, 'i') } }
+        ]
+      }
+    };
+  }
 
-    return Rental.aggregate(aggregationStages);
-}
+  let searchByUserStage = {};
+  if (parsedQuery.userSearch) {
+    searchByBookStage = {
+      $match: {
+        $or: [
+          { firstName: { $regex: new RegExp(parsedQuery.userSearch, 'i') } },
+          { lastName: { $regex: new RegExp(parsedQuery.userSearch, 'i') } },
+          { phoneNumber: { $regex: new RegExp(parsedQuery.userSearch, 'i') } },
+          { email: { $regex: new RegExp(parsedQuery.userSearch, 'i') } }
+        ]
+      }
+    };
+  }
+
+  let paginationStages = [];
+  if (parsedQuery.page && parsedQuery.limit) {
+    const page = Number(parsedQuery.page) || 1;
+    const limit = Number(parsedQuery.limit) || 100;
+    const toSkip = (page - 1) * limit;
+    paginationStages.push({
+      $facet: {
+        paginatedResults: [
+          { $skip: toSkip },
+          { $limit: limit }
+        ],
+        totalCount: [
+          { $count: 'count' }
+        ]
+      }
+    });
+    paginationStages.push({
+      $unwind: {
+        path: '$totalCount'
+      }
+    });
+  }
+
+
+  const aggregationStages = [];
+  // 1. Match only logged-in user if not admin
+  if (user.role === 'user') {
+    if (!isEmptyObject(userIdStage)) aggregationStages.push(userIdStage);
+  }
+  // 2. Filter by status and date
+  if (!isEmptyObject(filterByStatusStage)) aggregationStages.push(filterByStatusStage);
+  if (!isEmptyObject(filterByStartDateStage)) aggregationStages.push(filterByStartDateStage);
+  if (!isEmptyObject(filterByReturnDateStage)) aggregationStages.push(filterByReturnDateStage);
+
+  // 3. Join books
+  aggregationStages.push(...joinBooksStage);
+  aggregationStages.push(...projectBookFieldsStage);
+
+  // 4. Join user data
+  aggregationStages.push(...joinUsersStage);
+  aggregationStages.push(...projectUsersFieldsStage);
+
+  // 5. Search by book title
+  if (!isEmptyObject(searchByBookStage)) aggregationStages.push(searchByBookStage);
+
+  // 6. Search by user
+  if (!isEmptyObject(searchByUserStage)) aggregationStages.push(searchByUserStage);
+
+  // 7. Paginate
+  aggregationStages.push(...paginationStages);
+
+
+  return Rental.aggregate(aggregationStages);
+};
 export default rentalAggregation;
--- controllers/rentalController.mjs
+++ controllers/rentalController.mjs
@@ -1,206 +1,200 @@
-import {catchAsync} from '../utils/catchAsync.mjs';
-import {AppError} from './../utils/appError.mjs';
-import {filterObject} from './../utils/filterObject.mjs';
+import { catchAsync } from '../utils/catchAsync.mjs';
+import { AppError } from './../utils/appError.mjs';
+import { filterObject } from './../utils/filterObject.mjs';
 
 import Rental from './../models/rental.mjs';
 import User from './../models/user.mjs';
 import Book from './../models/book.mjs';
-import QueryFeatures from '../utils/queryFeatuers.mjs';
-import isEmptyObject from '../utils/isEmptyObject.mjs';
 import rentalAggregation from '../aggregations/rentalAggregation.mjs';
 import createPaginationObject from '../utils/createPaginationObject.mjs';
+
+
+const sendRentalData = async function(req, res, next) {
+  const rentalsData = await rentalAggregation(req.query, req.user);
+
+  if (!rentalsData || rentalsData.length === 0) {
+    return next(new AppError('Rentals for logged in user with specified filters not found', 404));
+  }
+
+  const rentals = rentalsData[0].paginatedResults;
+  const total = rentalsData[0].totalCount.count;
+
+  if (!rentals) {
+    return next(new AppError('Rentals for logged in user not found', 404));
+  }
+
+  if (!req.query.page && !req.query.limit) {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        rentals
+      }
+    });
+    return;
+  }
+
+  const pagination = createPaginationObject(Number(req.query.page), Number(req.query.limit), total, rentals.length);
+  console.log(pagination);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      rentals,
+      pagination
+    }
+  });
+};
 
 const rentalController = {};
 
 rentalController.getAllRentals = catchAsync(async function(req, res, next) {
-    // Requires conversion to aggregation
-    const features = new QueryFeatures(Rental.find(), req.query);
-    await features.filter().sort().limitFields().paginate();
-    const rentals = await features.query;
+  // Requires conversion to aggregation
+  await sendRentalData(req, res, next);
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            rentals
-        }
-    });
 });
 
 rentalController.getRental = catchAsync(async function(req, res, next) {
-    const {id} = req.params;
-    const rental = await Rental.findById(id);
-    if (!rental)
-        return next(new AppError('Rental with specified ID not found', 404));
+  const { id } = req.params;
+  const rental = await Rental.findById(id);
+  if (!rental)
+    return next(new AppError('Rental with specified ID not found', 404));
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            rental
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      rental
+    }
+  });
 });
 
 rentalController.createRental = catchAsync(async function(req, res, next) {
-    const filteredBody = filterObject(req.body, 'user', 'book');
+  const filteredBody = filterObject(req.body, 'user', 'book');
 
-    // Check if the book is available
-    const bookId = filteredBody.book;
-    const book = await Book.findById(bookId);
-    if (!book) {
-        return next(new AppError('Book with specified ID not found', 404));
+  // Check if the book is available
+  const bookId = filteredBody.book;
+  const book = await Book.findById(bookId);
+  if (!book) {
+    return next(new AppError('Book with specified ID not found', 404));
+  }
+
+  if (book.currentStatus !== 'available')
+    return next(new AppError('Book is not available', 406));
+
+  // Check if the user is eligible
+  const userId = filteredBody.user;
+  const user = await User.findById(userId);
+  if (!user.eligible)
+    return next(
+      new AppError(
+        'User is not eligible to borrow any more books. Return borrowed ones first',
+        406
+      )
+    );
+
+  // Create rental, append refference to user, mark book as borrowed
+  const rental = await Rental.create(filteredBody);
+  const rentalId = rental._id;
+
+  user.rentals.push(rentalId);
+  user.save();
+  book.currentStatus = 'borrowed';
+  book.save();
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      rental
     }
-
-    if (book.currentStatus !== 'available')
-        return next(new AppError('Book is not available', 406));
-
-    // Check if the user is eligible
-    const userId = filteredBody.user;
-    const user = await User.findById(userId);
-    if (!user.eligible)
-        return next(
-            new AppError(
-                'User is not eligible to borrow any more books. Return borrowed ones first',
-                406
-            )
-        );
-
-    // Create rental, append refference to user, mark book as borrowed
-    const rental = await Rental.create(filteredBody);
-    const rentalId = rental._id;
-
-    user.rentals.push(rentalId);
-    user.save();
-    book.currentStatus = 'borrowed';
-    book.save();
-
-    res.status(201).json({
-        status: 'success',
-        data: {
-            rental
-        }
-    });
+  });
 });
 
 rentalController.updateRental = catchAsync(async function(req, res, next) {
-    const filteredBody = filterObject(req.body, 'currentStatus');
+  const filteredBody = filterObject(req.body, 'currentStatus');
 
-    if (filteredBody.currentStatus !== 'returned')
-        return next(
-            new AppError(
-                'This route is dedicated to marking books as returned.\n Use "currentStatus": "returned"',
-                405
-            )
-        );
+  if (filteredBody.currentStatus !== 'returned')
+    return next(
+      new AppError(
+        'This route is dedicated to marking books as returned.\n Use "currentStatus": "returned"',
+        405
+      )
+    );
 
-    // Mark the rental with the correct status
-    const {id} = req.params;
-    const rental = await Rental.findById(id);
-    if (!rental)
-        return next(new AppError('Rental with specified ID not found', 404));
-    if (rental.currentStatus === 'returned')
-        return next(new AppError('This book has been already returned', 405));
-    rental.currentStatus = filteredBody.currentStatus;
-    rental.returnDate = Date.now();
-    rental.save();
+  // Mark the rental with the correct status
+  const { id } = req.params;
+  const rental = await Rental.findById(id);
+  if (!rental)
+    return next(new AppError('Rental with specified ID not found', 404));
+  if (rental.currentStatus === 'returned')
+    return next(new AppError('This book has been already returned', 405));
+  rental.currentStatus = filteredBody.currentStatus;
+  rental.returnDate = Date.now();
+  rental.save();
 
-    // Mark the book with the correct status
-    const bookId = rental.book;
-    const book = await Book.findById(bookId);
+  // Mark the book with the correct status
+  const bookId = rental.book;
+  const book = await Book.findById(bookId);
 
-    const userId = rental.user;
-    const user = await User.findById(userId);
-    if (book.currentStatus == 'lost') user.eligible('true');
+  const userId = rental.user;
+  const user = await User.findById(userId);
+  if (book.currentStatus == 'lost') user.eligible('true');
 
-    book.currentStatus = 'available';
-    book.save();
+  book.currentStatus = 'available';
+  book.save();
 
-    // Remove reference from user rental array
-    const result = user.rentals;
-    const index = result.indexOf(rental._id);
-    result.splice(index, 1);
-    user.rentals = result;
-    await user.save();
+  // Remove reference from user rental array
+  const result = user.rentals;
+  const index = result.indexOf(rental._id);
+  result.splice(index, 1);
+  user.rentals = result;
+  await user.save();
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            rental
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      rental
+    }
+  });
 });
 rentalController.deleteRental = catchAsync(async function(req, res, next) {
-    const {id} = req.params;
-    const rental = await Rental.findByIdAndDelete(id);
-    if (!rental)
-        return next(new AppError('Rental with specified ID not found', 404));
+  const { id } = req.params;
+  const rental = await Rental.findByIdAndDelete(id);
+  if (!rental)
+    return next(new AppError('Rental with specified ID not found', 404));
 
-    res.status(204).json({
-        status: 'success',
-        data: null
-    });
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
 });
 
 rentalController.getBookHistory = catchAsync(async function(req, res, next) {
-    const bookId = req.params.id;
-    const rentals = await Rental.find({book: bookId});
-    if (!rentals) {
-        return next(new AppError('Rentals for book with specified ID not found', 404));
-    }
+  const bookId = req.params.id;
+  const rentals = await Rental.find({ book: bookId });
+  if (!rentals) {
+    return next(new AppError('Rentals for book with specified ID not found', 404));
+  }
 
-    res.status(200).json({
-        status: 'success',
-        data: rentals
-    });
+  res.status(200).json({
+    status: 'success',
+    data: rentals
+  });
 });
 
 rentalController.getUserHistory = catchAsync(async function(req, res, next) {
-    const userId = req.params.id;
-    const rentals = await Rental.find({user: userId});
-    if (!rentals) {
-        return next(new AppError('Rentals for user with specified ID not found', 404));
-    }
+  const userId = req.params.id;
+  const rentals = await Rental.find({ user: userId });
+  if (!rentals) {
+    return next(new AppError('Rentals for user with specified ID not found', 404));
+  }
 
-    res.status(200).json({
-        status: 'success',
-        data: rentals
-    });
+  res.status(200).json({
+    status: 'success',
+    data: rentals
+  });
 });
 
 rentalController.getLoggedInUserHistory = catchAsync(async function(req, res, next) {
-    // Requires conversion to aggregation
-    const userId = req.user._id;
-    const rentalsData = await rentalAggregation(['title'], req.query, req.user);
-
-    if (!rentalsData || rentalsData.length === 0) {
-        return next(new AppError('Rentals for logged in user with specified filters not found', 404));
-    }
-
-    const rentals = rentalsData[0].paginatedResults;
-    const total = rentalsData[0].totalCount.count;
-
-    if (!rentals) {
-        return next(new AppError('Rentals for logged in user not found', 404));
-    }
-
-    if (!req.query.page && !req.query.limit) {
-        res.status(200).json({
-            status: 'success',
-            data: {
-                rentals
-            }
-        });
-        return;
-    }
-
-    const pagination = createPaginationObject(Number(req.query.page), Number(req.query.limit), total, rentals.length);
-    console.log(pagination);
-    res.status(200).json({
-        status: 'success',
-        data: {
-            rentals,
-            pagination
-        }
-    });
+  // Requires conversion to aggregation
+  await sendRentalData(req, res, next);
 });
 
 export default rentalController;
